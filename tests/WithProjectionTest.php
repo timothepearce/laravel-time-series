f4173ef2a8f3d1a3d75c6b2807bb8706ac80084b
Timothé Pearce <timothe.pearce@gmail.com>
2021-09-01 10:02:34 +0200

    Add type to var

--- tests/WithProjectionTest.php
+++ tests/WithProjectionTest.php
@@ -9,6 +9,7 @@ class WithProjectionTest extends TestCase
     /** @test */
     public function it_creates_a_projection_for_each_interval_when_a_model_with_projections_is_created()
     {
+        /** @var Log $log */
         $log = Log::factory()->create();
         $numberOfIntervals = $log->getIntervalCount();
 
