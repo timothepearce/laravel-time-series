b428756c6de7373808a4f99794cb4d243e29e218
Timothé Pearce <timothe.pearce@gmail.com>
2021-09-08 02:36:28 +0200

    Writes collection tests

--- src/ProjectionCollection.php
+++ src/ProjectionCollection.php
@@ -3,6 +3,9 @@
 namespace Laravelcargo\LaravelCargo;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Carbon;
+use Laravelcargo\LaravelCargo\Models\Projection;
+use Laravelcargo\LaravelCargo\Tests\Models\Log;
 
 class ProjectionCollection extends Collection
 {
